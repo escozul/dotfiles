89eb7113227d5c1524e25efceb8a6cbc3f8137cb
Maria Avgeri <escozul@gmail.com>
2026-05-01 16:18:54 +0300

    Restore help output aliases for bat
    Re-enable colorized help output via bat and restore aliases for help commands.

--- dotfiles/dot_shell/sourced/third-party/bat.sh
+++ dotfiles/dot_shell/sourced/third-party/bat.sh
@@ -2,7 +2,21 @@
 export BAT_THEME="DarkNeon"
 
 # Pass all help (-h, --help) output through bat
+# This Caused a lot of issues that used actual -h arguments like du -h So I removed it.
+# if [[ -n ${ZSH_NAME} ]]; then
+#     alias -g -- -h='-h 2>&1 | bat --language=help --style=plain'
+#     alias -g -- --help='--help 2>&1 | bat --language=help --style=plain'
+# fi
+
+# Colorized help output via bat
 if [[ -n ${ZSH_NAME} ]]; then
     alias -g -- -h='-h 2>&1 | bat --language=help --style=plain'
     alias -g -- --help='--help 2>&1 | bat --language=help --style=plain'
+    help() { "$@" --help 2>&1 | bat --language=help --style=plain; }
+
+    # Commands where -h means human-readable, not help
+    for cmd in du df ls free curl wc sort cut head tail numfmt; do
+        alias $cmd="command $cmd"
+    done
 fi
+
