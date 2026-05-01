99ab9ae8fb7d61683675de85b0291ff2c87cd8f9
Maria Avgeri <escozul@gmail.com>
2026-05-01 16:26:51 +0300

    Rename help function to bathelp for clarity

--- dotfiles/dot_shell/sourced/third-party/bat.sh
+++ dotfiles/dot_shell/sourced/third-party/bat.sh
@@ -12,7 +12,7 @@ export BAT_THEME="DarkNeon"
 if [[ -n ${ZSH_NAME} ]]; then
     alias -g -- -h='-h 2>&1 | bat --language=help --style=plain'
     alias -g -- --help='--help 2>&1 | bat --language=help --style=plain'
-    help() { "$@" --help 2>&1 | bat --language=help --style=plain; }
+    bathelp() { "$@" --help 2>&1 | bat --language=help --style=plain; }
 
     # Commands where -h means human-readable, not help
     for cmd in du df ls free curl wc sort cut head tail numfmt; do
